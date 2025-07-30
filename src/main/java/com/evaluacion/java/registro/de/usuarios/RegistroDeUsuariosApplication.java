0c7c199473a16b5b98e0b8fa472b282f14e6f195
chris <chriskhun123@gmail.com>
2025-07-30 00:01:15 -0400

    Proyecto inicializado.

---
+++ src/main/java/com/evaluacion/java/registro/de/usuarios/RegistroDeUsuariosApplication.java
@@ -0,0 +1,14 @@
+package com.evaluacion.java.registro.de.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroDeUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroDeUsuariosApplication.class, args);
+		System.out.println("Hola Mundo");
+	}
+
+}
---
+++ src/test/java/com/evaluacion/java/registro/de/usuarios/RegistroDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluacion.java.registro.de.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
