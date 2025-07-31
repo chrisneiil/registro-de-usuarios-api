ed15e3d5ab6c365906c59ec5cdcde5e789496715
chris <chriskhun123@gmail.com>
2025-07-31 02:17:26 -0400

    hot-fix main reintegrado.

---
+++ src/main/java/com/evaluacion/java/registroDeUsuariosApi/RegistroDeUsuariosApiApplication.java
@@ -0,0 +1,13 @@
+package com.evaluacion.java.registroDeUsuariosApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroDeUsuariosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroDeUsuariosApiApplication.class, args);
+	}
+
+}
